daf48e01d34a5368bd090a51ae1208a449736884
Zak <11335229+DontKnowZak@users.noreply.github.com>
2024-10-10 15:46:46 +0100

    Add ability to change status to project show page
    This commit allows the user to edit the status of the project from the project page.
    They can choose between the statuses of 'Todo', 'In Progress', and 'Done'.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_project, only: %i[show]
+  before_action :set_project, only: %i[show update]
 
   def index
     @projects = Project.all
@@ -11,9 +11,26 @@ class ProjectsController < ApplicationController
     @status_changes = @project.status_changes.order(created_at: :desc)
   end
 
+  def update
+    if @project.update(project_params)
+      if @project.previous_changes[:status]
+        StatusChange.create!(status: @project.status, user: current_user, project: @project)
+        redirect_to @project, notice: "Project status updated."
+      end
+    else
+      @comments = @project.comments.order(created_at: :desc)
+      @status_changes = @project.status_changes.order(created_at: :desc)
+      render :show
+    end
+  end
+
   private
 
   def set_project
     @project = Project.find(params[:id])
+  end
+
+  def project_params
+    params.require(:project).permit(:status)
   end
 end
